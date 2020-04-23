3262165090d5e532d9e0e9ce6660086e548062d0
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-23 15:45:50 -0500

    convert Header from function to class components

--- newsapp/src/components/Header.js
+++ newsapp/src/components/Header.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 
-function Header() {
-    return (
-      <div>
+// function Header() {   //use function component for testing
+//     return (
+//       <div>
     
-        <h1 className="header"> Google News App</h1>
+//         <h1 className="header"> Google News App</h1>
   
-      </div>
-    );
+//       </div>
+//     );
+//   }
+
+
+  class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                
+                <h1 className="header"> Google News App</h1>
+  
+            </div>
+        )
+    }
   }
   
   export default Header
