afef098df323a5695ce2e3356a3a9444edf80d0a
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-23 09:29:01 -0500

    set up blank function for Header

---
+++ newsapp/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+    return (
+      <div className="App">
+        
+     <h1> test on May, 2020</h1>
+  
+      </div>
+    );
+  }
+  
+  export default Header;
+  
