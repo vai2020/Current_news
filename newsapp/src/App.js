664310159ed96557b8af891c4dd03b88509a9a48
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-24 10:07:16 -0500

    import router in App.js

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from "axios";
+import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Us from "./components/Us";
