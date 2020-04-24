15c81d8e53d4a430e5d3a2f38936be2ea02564ea
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-24 01:34:05 -0500

    add <div> to Header in App.js

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   render() {
       return (
           <div>
-              <Header />
+              <div><Header /></div>
               <Main />
                <Us />
                <World />
