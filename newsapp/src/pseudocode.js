a6f75d3d054cda1bddb3c3582dda6a8ac8d774a5
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-24 22:48:25 -0500

    update pseudocode file

--- newsapp/src/pseudocode.js
+++ newsapp/src/pseudocode.js
@@ -1,1 +1,17 @@
-xxxxxxx
+import App from "./App"
+
+Pseudocode:
+1) npx create-react-app to start the newsapp
+2) npm install axios
+3) npm install --save react-router-dom
+4) set up 2 class components 
+5) set up 5 functional components
+6) style in App.css
+7) make sure to import all the components in App.js
+8) set up 3 routes for US, World and Main content news
+9) set up async actions to send data to and receive data from a server
+10)uses CSS to style application
+
+
+Reference videos:
+1) 
